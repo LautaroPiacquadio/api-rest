781294496e82715e4ecb8d8cefc66d80c166dba4
Lautaro Piacquadio <lautaropiacquadio.lp@gmail.com>
2017-08-02 22:27:53 -0300

    eslint

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-var express = require('express')
-var cors = require('cors')
-var path = require('path')
-var favicon = require('serve-favicon')
-var logger = require('morgan')
-var mongoose = require('mongoose')
-var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
-var config = require('./lib/config')
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const config = require('./lib/config')
 
-var app = express()
+const app = express()
 
 // mongoose.createConnection(config.db)
 mongoose.connect(config.db)
 
-var auth_middle = require('./lib/middleware/auth')
-var index = require('./routes/index')
-var auth = require('./routes/auth')
-var user = require('./routes/user')
-var movie = require('./routes/movie')
+const auth_middle = require('./lib/middleware/auth')
+const index = require('./routes/index')
+const auth = require('./routes/auth')
+const user = require('./routes/user')
+const movie = require('./routes/movie')
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-// app.use(cors({ origin: http://localhost:8000 }))
+app.use(cors({ origin: 'http://localhost:8080' }))
 
 // Rutas inseguras
 app.use('/', index)
@@ -39,10 +39,10 @@ app.use('/movie', movie)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found')
+    let err = new Error('Not Found')
     err.status = 404
     next(err)
-});
+})
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -51,6 +51,6 @@ app.use(function(err, req, res, next) {
     res.locals.error = req.app.get('env') === 'development' ? err : {}
 
     res.status(err.status || 500)
-});
+})
 
 module.exports = app
